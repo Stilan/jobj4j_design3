67b490856b3454ab58661567f17318373dd46c90
Alex <aleksandr.litvinov89qw@gmail.com>
2025-05-17 00:19:56 +0700

    1. Что такое обобщенные типы (generics)

---
+++ src/test/java/ru/job4j/generics/RoleStoreTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.generics;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class RoleStoreTest {
+
+    @Test
+    void whenAddAndFindThenRoleIsAdmin() {
+        RoleStore store = new RoleStore();
+        store.add(new Role("1", "Admin"));
+        Role result = store.findById("1");
+        assertThat(result.getRole()).isEqualTo("Admin");
+    }
+
+    @Test
+    void whenAddAndFindThenRoleAdminIsNull() {
+        RoleStore store = new RoleStore();
+        store.add(new Role("1", "Admin"));
+        Role result = store.findById("10");
+        assertThat(result).isNull();
+    }
+
+    @Test
+    void whenAddDuplicateAndFindUserRoseIsAdmin() {
+        RoleStore store = new RoleStore();
+        store.add(new Role("1", "Admin"));
+        store.add(new Role("1", "User"));
+        Role result = store.findById("1");
+        assertThat(result.getRole()).isEqualTo("Admin");
+    }
+}
