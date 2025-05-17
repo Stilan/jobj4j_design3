da893091c0be9703e10631f2fe325ebbde7e985e
Alex <aleksandr.litvinov89qw@gmail.com>
2025-05-17 20:08:24 +0700

    1. Динамический список на массиве.

---
+++ src/main/java/ru/job4j/collection/SimpleArrayList.java
@@ -0,0 +1,78 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class SimpleArrayList<T> implements SimpleList<T> {
+
+    private T[] container;
+    private int size;
+    private int modCount;
+
+    public SimpleArrayList(int capacity) {
+        container = (T[]) new Object[capacity];
+    }
+
+    @Override
+    public void add(T value) {
+        if (size == container.length) {
+            container = Arrays.copyOf(container, container.length == 0 ? 1 : container.length * 2);
+        }
+        container[size++] = value;
+        modCount++;
+    }
+
+    @Override
+    public T set(int index, T newValue) {
+        Objects.checkIndex(index, size);
+        T oldIndex = container[index];
+        container[index] = newValue;
+        return oldIndex;
+    }
+
+    @Override
+    public T remove(int index) {
+        Objects.checkIndex(index, size);
+        T t = get(index);
+        System.arraycopy(container, index + 1, container, index, size - index - 1);
+        size--;
+        modCount++;
+        return t;
+    }
+
+    @Override
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        return container[index];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+         return new Iterator<T>() {
+             int expectedModCount = modCount;
+             int index;
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return index < size;
+            }
+
+            @Override
+            public T next() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return container[index++];
+            }
+        };
+    }
+}
